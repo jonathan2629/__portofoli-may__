41383a63cc8f6b8bba3c254850fe58885b5e62ce
JONATHAN CACERES <jonathancaceres2629@gmail.com>
2023-05-18 19:22:07 -0500

    home commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const navLinksLi = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", () => {
+  // Toggle Nav
+  navLinks.classList.toggle("nav-active");
+
+  // Animate Links
+  navLinksLi.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+    }
+  });
+
+  // Burger Animation
+  burger.classList.toggle("toggle");
+});
